d62a36bc7269a385702142079e349db5e626dbcb
francosantzz <franco.santibanez@alumnos.frm.utn.edu.ar>
2023-11-01 21:57:46 -0300

    agrego forms

---
+++ Views/Main.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Views
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CargarAsiento = new System.Windows.Forms.Button();
+            this.LibroDiario = new System.Windows.Forms.Button();
+            this.LibroMayor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CargarAsiento
+            // 
+            this.CargarAsiento.Location = new System.Drawing.Point(80, 49);
+            this.CargarAsiento.Margin = new System.Windows.Forms.Padding(2);
+            this.CargarAsiento.Name = "CargarAsiento";
+            this.CargarAsiento.Size = new System.Drawing.Size(172, 35);
+            this.CargarAsiento.TabIndex = 1;
+            this.CargarAsiento.Text = "Cargar Asiento";
+            this.CargarAsiento.UseVisualStyleBackColor = true;
+            this.CargarAsiento.Click += new System.EventHandler(this.CargarAsiento_Click);
+            // 
+            // LibroDiario
+            // 
+            this.LibroDiario.Location = new System.Drawing.Point(80, 142);
+            this.LibroDiario.Margin = new System.Windows.Forms.Padding(2);
+            this.LibroDiario.Name = "LibroDiario";
+            this.LibroDiario.Size = new System.Drawing.Size(172, 35);
+            this.LibroDiario.TabIndex = 2;
+            this.LibroDiario.Text = "Libro Diario";
+            this.LibroDiario.UseVisualStyleBackColor = true;
+            this.LibroDiario.Click += new System.EventHandler(this.LibroDiario_Click);
+            // 
+            // LibroMayor
+            // 
+            this.LibroMayor.Location = new System.Drawing.Point(80, 238);
+            this.LibroMayor.Margin = new System.Windows.Forms.Padding(2);
+            this.LibroMayor.Name = "LibroMayor";
+            this.LibroMayor.Size = new System.Drawing.Size(172, 35);
+            this.LibroMayor.TabIndex = 3;
+            this.LibroMayor.Text = "Libro Mayor";
+            this.LibroMayor.UseVisualStyleBackColor = true;
+            this.LibroMayor.Click += new System.EventHandler(this.LibroMayor_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(329, 325);
+            this.Controls.Add(this.LibroMayor);
+            this.Controls.Add(this.LibroDiario);
+            this.Controls.Add(this.CargarAsiento);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Main";
+            this.Text = "Programa Contable";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button LibroDiario;
+        private System.Windows.Forms.Button LibroMayor;
+
+        private System.Windows.Forms.Button CargarAsiento;
+
+        #endregion
+    }
+}
---
+++ Views/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Views
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarAsiento_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Asiento asiento = new Asiento();
+            asiento.Show();
+        }
+
+        private void LibroDiario_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LibroDiario librodiario = new LibroDiario();
+            librodiario.Show();
+        }
+
+        private void LibroMayor_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LibroMayor libromayor = new LibroMayor();
+            libromayor.Show();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
