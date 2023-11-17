8420c70a6a72b4dc0c89bb2d087847c1c221c4e4
francosantzz <franco.santibanez@alumnos.frm.utn.edu.ar>
2023-11-17 08:53:44 -0300

    cambios

--- Data/Program.cs
+++ Data/Program.cs
@@ -45,30 +45,5 @@ namespace Data
             }
         }
         
-        
-        
-        
-        
-        
-        
-           /* var blockchain = new Blockchain();
-
-            Console.WriteLine(File.Exists("blockchain.json") ? "Created." : "Ups");
-
-            Account a1 = new Account(1, FinancialState.Activo, "Caja");
-            Account a2 = new Account(2, FinancialState.Activo, "Mercadería");
-
-            Movement m1 = new Movement(1000, 0, a1);
-            Movement m2 = new Movement(0, 1000, a2);
-            List<Movement> movements = new List<Movement>() { m1, m2 };
-
-            Seat seat = new Seat(DateTime.Now, movements);
-            List<Seat> seats = new List<Seat>() { seat };
-
-            Block block = new Block(1, DateTime.Now, seats,
-                "D835BA9BD7288F390C4A6CBAAD4A8BDD828FF5FA2418773F751E05845899E97B");
-            
-            blockchain.AddBlock(block);*/
-        
     }
 }
--- Models/Blockchain.cs
+++ Models/Blockchain.cs
@@ -11,77 +11,23 @@ namespace Data
     [Serializable]
     public class Blockchain
     {
-        //atributos
+        
         private List<Block> blocks = new List<Block>();
-        private List<Seat> tempAccount = new List<Seat>();
         //private const string PersistenceFile = "blockchain.json";
 
 
         //getters and setters
         public List<Block> Blocks { get => blocks; set => blocks = value; }
-        public List<Seat> _TempAccount { get => tempAccount; set => tempAccount = value; }
 
         public Blockchain()
         {
         }
 
-        public Blockchain(List<Block> blocks, List<Seat> tempAccount)
+        public Blockchain(List<Block> blocks)
         {
             Blocks = blocks;
-            _TempAccount = tempAccount;
-            /*if (File.Exists(PersistenceFile))
-            {
-                LoadBlockchainFromFile();
-            }
-            else
-            {
-                //AddGenesisBlock();
-            }*/
-        }
-        /*
-        private void LoadBlockchainFromFile()
-        {
-            try
-            {
-                var json = File.ReadAllText(PersistenceFile);
-                Blocks = JsonConvert.DeserializeObject<List<Block>>(json);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error loading blockchain from file: " + e.Message);
-            }
-        }
-
-        private void SaveBlockchainToFile()
-        {
-            try
-            {
-                var json = JsonConvert.SerializeObject(Blocks, (Newtonsoft.Json.Formatting)Formatting.Indented);
-                File.WriteAllText(PersistenceFile, json);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error saving blockchain to file: " + e.Message);
-            }
+            
         }
         
-        public void AddBlock(Block block)
-        {
-            Blocks.Add(block);
-            SaveBlockchainToFile();
-        }
-        
-        private void AddGenesisBlock()
-        {
-            var genesisBlock = new Block(0, DateTime.Now, null, "0");
-            Blocks.Add(genesisBlock);
-            SaveBlockchainToFile();
-        }*/
-        
-        
-        
-        
-        
-        //Esto comentado es pq estoy probando de persistir como lo hicieron los otros 
     }
 }
